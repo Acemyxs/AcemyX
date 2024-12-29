77f81a27096d5cd850a42cad67d8a06ed357a8ee
Phawaaz <phawaazakin@gmail.com>
2024-12-29 16:01:06 +0100

    Minor change

--- src/components/CTAcard.jsx
+++ src/components/CTAcard.jsx
@@ -12,8 +12,16 @@ export default function CTAcard() {
             className='absolute top-[0%] right-[50%] -translate-x-1/2 left-[50%] translate-y-1/4'
           />
         </div>
-        <img src='/Team.svg' alt='' className='absolute top-0 right-40 ' />
-        <img src='FAQ.svg' alt='' className='absolute left-0 top-[50%]' />
+        <img
+          src='/Team.svg'
+          alt=''
+          className='absolute top-0 right-40  hover:animate-bounce '
+        />
+        <img
+          src='FAQ.svg'
+          alt=''
+          className='absolute left-0 top-[50%] hover:md:animate-bounce'
+        />
         <img src='' alt='' />
       </div>
     </div>
