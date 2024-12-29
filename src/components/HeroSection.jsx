137d6c11f48390917b39579101050ed954595886
Phawaaz <phawaazakin@gmail.com>
2024-12-30 00:39:15 +0100

    i dont know what to do

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,21 +2,21 @@ import ButtonCta from "./ButtonCta";
 
 function HeroSection() {
   return (
-    <section id="home" className="relative h-[90vh]">
-      <h1 className="text-center text-7xl font-bold leading-tight w-[15ch] mx-auto mt-16 mb-8">
+    <section id='home' className='relative h-[90vh]'>
+      <h1 className='text-center text-7xl font-bold leading-tight w-[15ch] mx-auto mt-16 mb-8'>
         Elevate{" "}
-        <span className="text-orange-600">
+        <span className='text-orange-600'>
           your
           <br /> JAMB
         </span>{" "}
         scores with AcemyX.
       </h1>
-      <p className="text-center w-[55ch] text-gray-500 font-medium mx-auto mb-8">
+      <p className='text-center w-[55ch] text-gray-500 font-medium mx-auto mb-8'>
         Your trusted virtual learning platform to excel in exams and beyond.
         Explore study tools, practice questions, and personalized mentorship
         anytime, anywhere.
       </p>
-      <div className="flex justify-center">
+      <div className='flex justify-center'>
         <ButtonCta>Sign up for free</ButtonCta>
       </div>
     </section>
--- src/components/StatisticsSection.jsx
+++ src/components/StatisticsSection.jsx
@@ -2,47 +2,47 @@ import CountUp from "react-countup";
 
 export default function StatisticsSection() {
   return (
-    <section className="w-[80%] mx-auto pb-16">
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
-        <div className="flex flex-col items-center">
+    <section className='w-[80%] mx-auto pb-16'>
+      <div className='grid grid-cols-1 gap-8 md:grid-cols-4 sm:grid-cols-2 mx-auto'>
+        <div className='flex flex-col items-center'>
           <CountUp
-            className="text-5xl mb-2 font-bold"
+            className='text-5xl mb-2 font-bold'
             start={0}
             end={3}
-            suffix="k"
+            suffix='k'
             duration={2}
           />
-          <p className="font-medium">Students Enrolled</p>
+          <p className='font-medium'>Students Enrolled</p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className='flex flex-col items-center'>
           <CountUp
-            className="text-5xl mb-2 font-bold"
+            className='text-5xl mb-2 font-bold'
             start={0}
             end={500}
-            suffix="+"
+            suffix='+'
             duration={2}
           />
-          <p className="font-medium">Practice Quizzes & Exams</p>
+          <p className='font-medium'>Practice Quizzes & Exams</p>
         </div>{" "}
-        <div className="flex flex-col items-center">
+        <div className='flex flex-col items-center'>
           <CountUp
-            className="text-5xl mb-2 font-bold"
+            className='text-5xl mb-2 font-bold'
             start={0}
             end={300}
-            suffix="k"
+            suffix='k'
             duration={2}
           />
-          <p className="font-medium">Curated Questions</p>
+          <p className='font-medium'>Curated Questions</p>
         </div>{" "}
-        <div className="flex flex-col items-center">
+        <div className='flex flex-col items-center'>
           <CountUp
-            className="text-5xl mb-2 font-bold"
+            className='text-5xl mb-2 font-bold'
             start={0}
             end={99}
-            suffix=".9%"
+            suffix='.9%'
             duration={2}
           />
-          <p className="font-medium">Uptime for 24/7 Access</p>
+          <p className='font-medium'>Uptime for 24/7 Access</p>
         </div>
       </div>
     </section>
