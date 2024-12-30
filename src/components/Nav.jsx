081c1c7936947e26a8ece0a637fb83313bb1e693
Phawaaz <phawaazakin@gmail.com>
2024-12-30 11:49:45 +0100

    mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,17 +2,19 @@ export default function Nav() {
   const navLinks = ["home", "features", "pricing", "testimonial"];
   return (
     <nav>
-      <ul className="flex gap-12 items-center">
+      <ul className='flex gap-12 items-center'>
         {navLinks.map((link) => (
           <li key={link}>
-            <a href={`#${link}`} className="capitalize font-medium">
+            <a href={`#${link}`} className='capitalize font-medium'>
               {link}
             </a>
           </li>
         ))}
-        <button className="inline-flex items-center gap-2 rounded-full py-3 px-5 border-2 text-orange-600 border-orange-600 hover:text-white font-medium hover:bg-orange-600 transition-all duration-300 shadow-sm hover:shadow-md">
-          <span>Sign up</span>
-          <img src="/Arrowup.svg" alt="arrow" className="w-6 h-6" />
+        <button className='inline-flex items-center gap-2 rounded-full py-3 px-5 border-2 text-orange-600 border-orange-600 hover:text-white font-medium hover:bg-orange-600 transition-all duration-300 shadow-sm hover:shadow-md'>
+          <span>
+            <a href='https://academy.acemyx.com/'>Sign up</a>
+          </span>
+          <img src='/Arrowup.svg' alt='arrow' className='w-6 h-6' />
         </button>
       </ul>
     </nav>
