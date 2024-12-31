2071b880d64f1946e0f80b7c082dfaf9472b01d0
Phawaaz <phawaazakin@gmail.com>
2024-12-31 11:51:57 +0100

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,7 @@
 export default function Logo() {
   return (
     <div className='flex items-center'>
-      <img
-        src='/acemyx-logo.webp'
-        alt='AcemyX Logo'
-        className='h-8 md:h-auto w-auto'
-      />
+      <img src='/acemyx-logo.webp' alt='AcemyX Logo' className='h-8 ' />
     </div>
   );
 }
