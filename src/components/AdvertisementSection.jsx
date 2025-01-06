44188705ff84dc6527a2a8228616993ad319b22b
Misbau-4 <abduldamilare14@gmail.com>
2025-01-06 23:29:48 +0100

    anotherPixel

--- src/components/AdvertisementSection.jsx
+++ src/components/AdvertisementSection.jsx
@@ -1,15 +1,15 @@
 function PromotionalSection() {
   return (
     <section className="w-[80%] mx-auto py-32">
-      <div className="grid [750px]:grid-cols-1 lg:grid-cols-2 gap-8 [805]:grid-cols-2">
-        <div className="grid gap-6 grid-rows-[1.5fr_1fr]">
-          <div className="bg-neutral-white-60 px-6 py-10 rounded-[48px]">
+      <div className="grid [750px]:grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8 [805]:grid-cols-2">
+        <div className="grid gap-4 lg:gap-6 grid-rows-[1.5fr_1fr]">
+          <div className="bg-neutral-white-60 px-8 md:px-6 py-10 rounded-[48px]">
             <p className="text-sm md:text-title_2 lg:text-title_1 font-medium md:w-[30ch] mx-auto">
               {`Whether you're facing high-stake exams or simply trying to ace your
             courses, we've got you covered.`}
             </p>
           </div>
-          <div className="grid grid-cols-2 gap-6">
+          <div className="grid grid-cols-2 gap-4 lg:gap-6">
             <div className="bg-text-color text-neutral-white-10 lg:px-12 lg:py-8 md:py-7 md:px-8 p-7 rounded-[32px] md:rounded-[40px] lg:rounded-[48px]">
               <p className="text-sm md:text-title_2 lg:text-title_1 font-medium mb-2">
                 Boost Exam <br /> Confidence
